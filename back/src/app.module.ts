769e3bfa8d12861fa2a63b3d0aa8b80eb6217451
Nafas Ebrahimi <ebranafas@gmail.com>
2025-10-02 22:13:15 +0330

    chore(backend): implement structure

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
+import { UserModule } from './user/user.module';
+import { TaskController } from './task/task.controller';
+import { TaskModule } from './task/task.module';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule, TaskModule, AuthModule],
+  controllers: [AppController, UserController, TaskController, AuthController],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
